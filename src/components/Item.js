ec3ed64015336a4966e72550d7e30538ff754f8a
andrea-hidalgo <andrea.hidalgo90@gmail.com>
2021-04-03 12:20:36 -0400

    optimized UseEffect hook

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,20 +5,21 @@ import { TextField } from '@material-ui/core';
 export default function Item ({index, remove, InputField}) {
     const { values, setFieldValue } = useFormikContext()
     
+    const qty=values.items[index].quantity;
+    const price=values.items[index].price;
 
     useEffect(() => {
-        const total = values.items[index].quantity * values.items[index].price
+        const total = qty * price;
         const accurateRound = Number(Math.round(total + "e2") + "e-2")
-        console.log(accurateRound);
         setFieldValue(`items.${index}.total`, accurateRound || '0')
-    }, [values.items[index].quantity, values.items[index].price])
+    }, [qty, price])
 
     return (
         <div key={index} className="invoiceItemContainer">
             <InputField name={`items.${index}.name`} placeholder="Service rendered" type="input" label="Item Name"/>
             <InputField name={`items.${index}.quantity`} placeholder="1" type="number" label="Qty."/>
             <InputField name={`items.${index}.price`} placeholder="0" type="number" label="Price"/>
-            <Field name={`items.${index}.total`} placeholder="0" type="number" disabled label="Total" as={TextField} variant="outlined"/>
+            <Field name={`items.${index}.total`} placeholder="0" type="number" disabled label="Total" as={TextField} variant="standard"/>
             <button type="button" onClick={() => remove(index)}>X</button>
         </div>
     )

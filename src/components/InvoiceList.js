7ee1c0ce3e723f3fa904bf8f4ec2d7ec4ab2400a
andrea-hidalgo <andrea.hidalgo90@gmail.com>
2021-04-03 22:17:12 -0400

    update function date to dueDate for easier reading

--- src/components/InvoiceList.js
+++ src/components/InvoiceList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function InvoiceList(props) {
 
-    const date = (invoice) => {
+    const dueDate = (invoice) => {
         const newDate = new Date(invoice.paymentDue);
         const formatDate = newDate.toDateString();
         const sliceDate = formatDate.slice(3);
@@ -15,7 +15,7 @@ export default function InvoiceList(props) {
                 return (
                     <div key={index}>
                         <h4>#{invoice.invoiceId}</h4>
-                        <p>Due {date(invoice)}</p>
+                        <p>Due {dueDate(invoice)}</p>
                         <p>{invoice.clientName}</p>
                         <h3>${invoice.total}</h3>
                         <h3>{invoice.status}</h3>

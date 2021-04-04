9f371c80b16f0e687de771943de9a881261d6782
andrea-hidalgo <andrea.hidalgo90@gmail.com>
2021-04-03 22:36:07 -0400

    updated schema to include state in form address

--- models/invoice.js
+++ models/invoice.js
@@ -3,6 +3,7 @@ const { Schema, model } = require('mongoose');
 const invoiceSchema = new Schema({
     invoiceId: String,
     paymentDue: {type:Date, default: Date.now},
+    createdAt: String,
     description: String,
     paymentTerms: Number,
     clientName: String,
@@ -11,12 +12,14 @@ const invoiceSchema = new Schema({
     senderAddress: {
         street: String,
         city: String,
+        state: String,
         zipCode: String,
         country: String
     },
     clientAddress: {
         street: String,
         city: String,
+        state: String,
         zipCode: String,
         country: String
     },

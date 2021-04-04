433a725120b6ceb95bb4b2ee0b3770242d6265ff
andrea-hidalgo <andrea.hidalgo90@gmail.com>
2021-04-03 22:14:35 -0400

    formatted displayed due date

--- src/components/InvoiceList.js
+++ src/components/InvoiceList.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
 export default function InvoiceList(props) {
+
+    const date = (invoice) => {
+        const newDate = new Date(invoice.paymentDue);
+        const formatDate = newDate.toDateString();
+        const sliceDate = formatDate.slice(3);
+        return sliceDate;
+    }
+
     return (
         <div>
             {props.invoiceData.map((invoice,index) =>{
                 return (
                     <div key={index}>
                         <h4>#{invoice.invoiceId}</h4>
-                        <p>Due {invoice.paymentDue}</p>
+                        <p>Due {date(invoice)}</p>
                         <p>{invoice.clientName}</p>
                         <h3>${invoice.total}</h3>
                         <h3>{invoice.status}</h3>

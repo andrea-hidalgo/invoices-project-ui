a05dab8b4f7cec789628c98780481ce884654f3c
andrea-hidalgo <andrea.hidalgo90@gmail.com>
2021-04-07 22:13:12 -0400

    sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Sidebar (props) {
+    return (
+        <div className="sidebar-container">
+            <div className="logo"></div>
+            <div>
+                <div className="theme-switch"></div>
+                <div className="user-picture"></div>
+            </div>
+        </div>
+    )
+}

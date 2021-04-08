6b42646dc3dcab1c27d10e97c003ef9c7c78fe75
andrea-hidalgo <andrea.hidalgo90@gmail.com>
2021-04-07 22:06:28 -0400

    added invoice counter to top of page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
 export default function Header (props) {
-
+  const countInvoices = () => {
+    if (props.invoicesData.length > 0) {
+      let count = 0;
+      props.invoicesData.forEach(invoice => count++)
+      return count + ' total';
+    } else {
+      return 'no'
+    }
+  }
     return (
         <header>
         <div className="header-left">
           <h1>Invoices</h1>
-          <p className="body1">There are X total invoices</p>
+          <p className="body1">There are {countInvoices()} invoices</p>
         </div>
         <div className="header-right">
           <button onClick={()=>props.toggleInvoiceHide()}>+ New Invoice</button>
